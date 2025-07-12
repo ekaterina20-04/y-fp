8b56e52e075a0d8de387b7dfba0cad5f6a0a5212
Kate <vodopianovae.com>
2025-07-12 14:24:53 +0300

    second is ready

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,64 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from "../tools/api";
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const isValidNumberString = (str) =>
+  /^[0-9]+(\.[0-9]+)?$/.test(str) && str.length > 2 && str.length < 10;
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
-
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
-
-     wait(2500).then(() => {
-         writeLog('SecondLog')
-
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
-
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  Promise.resolve(value)
+    .then((v) => {
+      writeLog(v);
+      return v;
+    })
+    .then((v) => {
+      if (!isValidNumberString(v) || parseFloat(v) <= 0) {
+        throw new Error("ValidationError");
+      }
+      return v;
+    })
+    .then((v) => {
+      const rounded = Math.round(parseFloat(v));
+      writeLog(rounded);
+      return rounded;
+    })
+    .then((num) =>
+      api.get("https://api.tech/numbers/base", {
+        number: num,
+        from: 10,
+        to: 2,
+      })
+    )
+    .then(({ result: binary }) => {
+      writeLog(binary);
+      return binary;
+    })
+    .then((binary) => {
+      const len = binary.length;
+      writeLog(len);
+      return len;
+    })
+    .then((len) => {
+      const sq = len * len;
+      writeLog(sq);
+      return sq;
+    })
+    .then((sq) => {
+      const mod3 = sq % 3;
+      writeLog(mod3);
+      return mod3;
+    })
+    .then((id) => api.get("https://animals.tech", { id }))
+    .then(({ result: animal }) => {
+      handleSuccess(animal);
+      console.log(animal);
+    })
+    .catch((err) => {
+      handleError(err.message);
+    });
+};
 
 export default processSequence;
